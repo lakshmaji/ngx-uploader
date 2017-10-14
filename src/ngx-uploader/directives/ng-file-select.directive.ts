9af5911cecf6f3a49c476631b69d130c83e30453
Ananth Mutyala <anantha@webileapps.com>
2017-10-14 15:57:44 +0530

    Remove event listner fix

--- src/ngx-uploader/directives/ng-file-select.directive.ts
+++ src/ngx-uploader/directives/ng-file-select.directive.ts
@@ -34,7 +34,9 @@ export class NgFileSelectDirective implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.el.removeEventListener('change', this.fileListener, false);
+    if(this.el) {
+      this.el.removeEventListener('change', this.fileListener, false);      
+    }
 
     if (this.uploadInput) {
       this.uploadInput.unsubscribe();
